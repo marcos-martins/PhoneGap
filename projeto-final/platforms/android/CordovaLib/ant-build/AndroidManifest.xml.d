0500c6b0955a576459f5cf53dbdbd5f55a3e8fd5
marcos-martins <m.martins@outlook.com>
2014-11-27 22:06:48 -0200

    Remoção app android

--- projeto-final/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Phonegap\Projetos\projeto-final\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Phonegap\Projetos\projeto-final\platforms\android\CordovaLib\AndroidManifest.xml \
--- projeto-final/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Phonegap\Projetos\projeto-final\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Phonegap\Projetos\projeto-final\platforms\android\AndroidManifest.xml \
